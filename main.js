b2d798eb05caf7416d10053e61d36c81dcb21f2d
Rayio2 <31609465+Rayio2@users.noreply.github.com>
2018-05-13 20:24:12 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,19 @@
-var allbuttons = $('#buttondiv>button')
+var allimgs=$('.imgs>img')
+$(allimgs[0]).addClass('current')
+$(allimgs[1]).addClass('right')
+$(allimgs[2]).addClass('right')
+var n=0
+setInterval(()=>{
+$(allimgs[n]).removeClass('current').addClass('left')
 
-var timerId = setTimer();
-var n = 0
+if(n===2) n = -1
+$(allimgs[n+1]).removeClass('right').addClass('current')
 
-function setTimer() {
-    return setInterval(() => {
-      n += 1
-      allbuttons.eq(n % 3).trigger('click')
-      if(n>=3)n=n%3
-    }, 1000)
-  }
-  
-var buttonAct = function(xxx){
-  $(allbuttons[xxx]).addClass('red')
-  $(allbuttons[xxx]).siblings().removeClass('red')
-}
-
-for(let i =0;i<allbuttons.length;i++)
-  {
-      $(allbuttons[i]).on('click',function(x)
-      {
-      var index = $(x.currentTarget).index()
-      var p = index * -300
-      buttonAct(index)
-      $('#container').css(
-        {
-          transform: 'translate('+ p +'px)'
-        })
-      })
-  }
-
-$('.window').on('mouseenter',function(){
-  window.clearInterval(timerId)
+$(allimgs[n]).one('transitionend',function(x)
+{
+ $(x.currentTarget).removeClass('left').addClass('right')
 })
 
-$('.window').on('mouseleave',function(){
-  timerId=setTimer() 
-})
+n=n+1
+
+},3000)
